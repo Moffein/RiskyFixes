c722906e43c0ea3d6fec8d2c48b9cb76d7c315d9
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-28 16:41:09 -0700

    Added artifact tweaks to source control

---
+++ RiskyFixes/Fixes/Artifacts/EnigmaBlacklistCheck.cs
@@ -0,0 +1,43 @@
+﻿using RoR2;
+using RoR2.Artifacts;
+using System;
+using System.Collections.Generic;
+
+namespace RiskyFixes.Fixes.Artifacts
+{
+    public class EnigmaBlacklistCheck : FixBase<EnigmaBlacklistCheck>
+    {
+        public override string ConfigCategoryString => "Artifacts";
+
+        public override string ConfigOptionName => "(Server-Side) Enigma Blacklist Fix";
+
+        public override string ConfigDescriptionString => "Fixes blacklisted equipment being able to spawn during Enigma runs.";
+
+        public override bool StopLoadOnConfigDisable => true;
+
+        protected override void ApplyChanges()
+        {
+            On.RoR2.Artifacts.EnigmaArtifactManager.OnRunStartGlobal += EnigmaArtifactManager_OnRunStartGlobal;
+        }
+
+        private void EnigmaArtifactManager_OnRunStartGlobal(On.RoR2.Artifacts.EnigmaArtifactManager.orig_OnRunStartGlobal orig, Run run)
+        {
+            orig(run);
+
+            //EnigmaArtifactManager.validEquipment
+            List<EquipmentIndex> toRemove = new List<EquipmentIndex>();
+            foreach (EquipmentIndex ei in EnigmaArtifactManager.validEquipment)
+            {
+                if (!Run.instance.availableEquipment.Contains(ei))
+                {
+                    toRemove.Add(ei);
+                }
+            }
+
+            foreach (EquipmentIndex ei in toRemove)
+            {
+                EnigmaArtifactManager.validEquipment.Remove(ei);
+            }
+        }
+    }
+}

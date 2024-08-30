5cf8c05e4820e8cc7ec61a12f1f0d85741b8ea1c
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-30 01:44:43 -0700

    Added DumpEntityStateConfig for future reference.

--- RiskyFixes/SneedUtils.cs
+++ RiskyFixes/SneedUtils.cs
@@ -6,6 +6,33 @@ namespace RiskyFixes
 {
     public static class SneedUtils
     {
+        public static void DumpEntityStateConfig(EntityStateConfiguration esc)
+        {
+
+            for (int i = 0; i < esc.serializedFieldsCollection.serializedFields.Length; i++)
+            {
+                if (esc.serializedFieldsCollection.serializedFields[i].fieldValue.objectValue)
+                {
+                    Debug.Log(esc.serializedFieldsCollection.serializedFields[i].fieldName + " - " + esc.serializedFieldsCollection.serializedFields[i].fieldValue.objectValue);
+                }
+                else
+                {
+                    Debug.Log(esc.serializedFieldsCollection.serializedFields[i].fieldName + " - " + esc.serializedFieldsCollection.serializedFields[i].fieldValue.stringValue);
+                }
+            }
+        }
+        public static void DumpEntityStateConfig(string entityStateName)
+        {
+            EntityStateConfiguration esc = LegacyResourcesAPI.Load<EntityStateConfiguration>("entitystateconfigurations/" + entityStateName);
+            DumpEntityStateConfig(esc);
+        }
+
+
+        public static void DumpAddressableEntityStateConfig(string addressablePath)
+        {
+            EntityStateConfiguration esc = Addressables.LoadAssetAsync<EntityStateConfiguration>(addressablePath).WaitForCompletion();
+            DumpEntityStateConfig(esc);
+        }
         public static bool SetAddressableEntityStateField(string fullEntityStatePath, string fieldName, string value)
         {
             EntityStateConfiguration esc = Addressables.LoadAssetAsync<EntityStateConfiguration>(fullEntityStatePath).WaitForCompletion();

f7b69c02bae46ac24315b8a3a0349cd441b6197a
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-30 19:51:09 -0700

    KillVoidTeam and SmallHoldoutCharging

---
+++ RiskyFixes/Fixes/General/SmallHoldoutCharging.cs
@@ -0,0 +1,45 @@
+﻿using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace RiskyFixes.Fixes.General
+{
+    public class SmallHoldoutCharging : FixBase<SmallHoldoutCharging>
+    {
+        public override string ConfigCategoryString => "General";
+
+        public override string ConfigOptionName => "(Server-Side) Small Holdout Charging";
+
+        public override string ConfigDescriptionString => "Small holdouts charge at full speed as long as 1 player is in the radius.";
+
+        protected override void ApplyChanges()
+        {
+            //I think only NullSafeWard and DropshipZone actually get changed by this.
+            string[] toFix = new string[]
+            {
+                "RoR2/Base/moon2/MoonBatteryBlood.prefab",
+                "RoR2/Base/moon2/MoonBatteryDesign.prefab",
+                "RoR2/Base/moon2/MoonBatteryMass.prefab",
+                "RoR2/Base/moon2/MoonBatterySoul.prefab",
+                "RoR2/DLC1/DeepVoidPortalBattery/DeepVoidPortalBattery.prefab",
+                "RoR2/Base/NullSafeWard/NullSafeWard.prefab",
+                "RoR2/Base/moon2/Moon2DropshipZone.prefab"
+            };
+            foreach (string str in toFix)
+            {
+                FixHoldout(Addressables.LoadAssetAsync<GameObject>(str).WaitForCompletion());
+            }
+        }
+
+        public static void FixHoldout(GameObject gameObject)
+        {
+            if (!gameObject) return;
+
+            HoldoutZoneController hzc = gameObject.GetComponentInChildren<HoldoutZoneController>();
+            if (hzc)
+            {
+                hzc.playerCountScaling = 0; //Power of 0 = 100% charge effectiveness at all times.
+            }
+        }
+    }
+}
---
+++ RiskyFixes/Fixes/Stages/Moon/KillVoidTeam.cs
@@ -0,0 +1,42 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.Networking;
+
+namespace RiskyFixes.Fixes.Stages.Moon
+{
+    public class KillVoidTeam : FixBase<KillVoidTeam>
+    {
+        public override string ConfigCategoryString => "Stages - Commencement";
+
+        public override string ConfigOptionName => "(Server-Side) Kill Void Team";
+
+        public override string ConfigDescriptionString => "Kills Void team enemies when the bossfight starts.";
+
+        protected override void ApplyChanges()
+        {
+            On.EntityStates.Missions.BrotherEncounter.BrotherEncounterBaseState.KillAllMonsters += BrotherEncounterBaseState_KillAllMonsters;
+        }
+
+        private void BrotherEncounterBaseState_KillAllMonsters(On.EntityStates.Missions.BrotherEncounter.BrotherEncounterBaseState.orig_KillAllMonsters orig, EntityStates.Missions.BrotherEncounter.BrotherEncounterBaseState self)
+        {
+            orig(self);
+
+            if (NetworkServer.active)
+            {
+                foreach (TeamComponent teamComponent in new List<TeamComponent>(TeamComponent.GetTeamMembers(TeamIndex.Void)))
+                {
+                    if (teamComponent)
+                    {
+                        HealthComponent component = teamComponent.GetComponent<HealthComponent>();
+                        if (component)
+                        {
+                            component.Suicide(null, null, DamageType.Generic);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

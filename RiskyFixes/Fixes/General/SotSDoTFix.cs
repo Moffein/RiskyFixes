75fd01cfc2726e70292cd40afef6d1a4dd813f41
Moffein <22742925+Moffein@users.noreply.github.com>
2025-01-23 07:50:51 -0600

    re-enabled dotfix

---
+++ RiskyFixes/Fixes/General/SotSDoTFix.cs
@@ -0,0 +1,40 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using System;
+using UnityEngine;
+
+namespace RiskyFixes.Fixes.General
+{
+    public class SotSDoTFix : FixBase<SotSDoTFix>
+    {
+        public override string ConfigCategoryString => "General";
+
+        public override string ConfigOptionName => "(Server-Side) DoT Fix";
+
+        public override string ConfigDescriptionString => "Fix the SotS bug where DoT stacks disappear randomly.";
+
+        protected override void ApplyChanges()
+        {
+            IL.RoR2.BurnEffectController.HandleDestroy += BurnEffectController_HandleDestroy;
+        }
+
+        private void BurnEffectController_HandleDestroy(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(x => x.MatchCall(typeof(UnityEngine.Object), "Destroy")))
+            {
+                c.Emit(OpCodes.Ldarg_0);//BurnEffectController
+                c.EmitDelegate<Func<UnityEngine.Object, BurnEffectController, UnityEngine.Object>>((orig, bec) =>
+                {
+                    return bec;
+                });
+            }
+            else
+            {
+                Debug.LogError("RiskyFixes: SotSDoTFix BurnEffectController_HandleDestroy IL Hook failed.");
+            }
+        }
+    }
+}
--- RiskyFixes/RiskyFixesPlugin.cs
+++ RiskyFixes/RiskyFixesPlugin.cs
@@ -19,7 +19,7 @@ namespace RiskyFixes
     [BepInDependency(R2API.R2API.PluginGUID)]
 
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
-    [BepInPlugin("com.Moffein.RiskyFixes", "RiskyFixes", "1.3.7")]
+    [BepInPlugin("com.Moffein.RiskyFixes", "RiskyFixes", "1.3.8")]
     public class RiskyFixesPlugin : BaseUnityPlugin
     {
         private void Awake()

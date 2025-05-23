5f78ad10c35acad6078eca123d38478032517c19
Moffein <22742925+Moffein@users.noreply.github.com>
2025-05-22 19:46:31 -0500

    Backup Fix, Harvest fix.

---
+++ RiskyFixes/Fixes/Minions/EquipmentSummonMaster.cs
@@ -0,0 +1,39 @@
+﻿using MonoMod.Cil;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RiskyFixes.Fixes.Minions
+{
+    public class EquipmentSummonMaster : FixBase<EquipmentSummonMaster>
+    {
+        public override string ConfigCategoryString => "Minions";
+
+        public override string ConfigOptionName => "(Server-Side) Equipment-Spawned Minions use Ambient Level";
+
+        public override string ConfigDescriptionString => "Fixes Back-Up drones not scaling with Ambient Level.";
+
+        protected override void ApplyChanges()
+        {
+            IL.RoR2.EquipmentSlot.SummonMaster += EquipmentSlot_SummonMaster;
+        }
+
+        private void EquipmentSlot_SummonMaster(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (c.TryGotoNext(x => x.MatchCallvirt<MasterSummon>("Perform")))
+            {
+                c.EmitDelegate<Func<MasterSummon, MasterSummon>>(summon =>
+                {
+                    summon.useAmbientLevel = true;
+                    return summon;
+                });
+            }
+            else
+            {
+                Debug.LogError("RiskyFixes: EquipmentSummonMaster IL hook failed.");
+            }
+        }
+    }
+}
---
+++ RiskyFixes/Fixes/Survivors/Treebot/HarvestAnim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyFixes.Fixes.Survivors.Treebot
+{
+    public class HarvestAnim : FixBase<HarvestAnim>
+    {
+        public override string ConfigCategoryString => "Survivors - REX";
+
+        public override string ConfigOptionName => "(Client-Side) DIRECTIVE: Harvest Animation";
+
+        public override string ConfigDescriptionString => "Restores the missing prep animation for DIRECTIVE: Harvest.";
+
+        protected override void ApplyChanges()
+        {
+            SneedUtils.SetAddressableEntityStateField("RoR2/Base/Treebot/EntityStates.Treebot.TreebotPrepFruitSeed.asset", "baseDuration", "0.1");
+        }
+    }
+}

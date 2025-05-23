4796f1e8f81b26a1b5c368271f4deba80f427c59
Moffein <22742925+Moffein@users.noreply.github.com>
2025-05-22 19:17:27 -0500

    Reprieve crit fix

---
+++ RiskyFixes/Fixes/Survivors/Seeker/ReprieveCritFix.cs
@@ -0,0 +1,43 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using RoR2.Projectile;
+using System;
+using UnityEngine;
+
+namespace RiskyFixes.Fixes.Survivors.Seeker
+{
+    public class ReprieveCritFix : FixBase<ReprieveCritFix>
+    {
+        public override string ConfigCategoryString => "Survivors - Seeker";
+        public override string ConfigOptionName => "(Server-Side) Reprieve Crit Fix";
+
+        public override string ConfigDescriptionString => "Fixes Reprieve not rolling for crits.";
+
+        protected override void ApplyChanges()
+        {
+            IL.RoR2.ReprieveVehicle.EndSojournVehicle += ReprieveVehicle_EndSojournVehicle;
+        }
+
+        private void ReprieveVehicle_EndSojournVehicle(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (c.TryGotoNext(x => x.MatchCallvirt<RoR2.Projectile.ProjectileManager>("FireProjectile")))
+            {
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<FireProjectileInfo, ReprieveVehicle, FireProjectileInfo>>((info, self) =>
+                {
+                    if (self.characterBody)
+                    {
+                        info.crit = self.characterBody.RollCrit();
+                    }
+                    return info;
+                });
+            }
+            else
+            {
+                Debug.LogError("RiskyFixes: Seeker ReprieveCritFix IL hook failed.");
+            }
+        }
+    }
+}

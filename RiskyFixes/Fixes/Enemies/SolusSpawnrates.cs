30180fcff56dc54a51c22196a0b5935c4f9b92bf
Moffein <22742925+Moffein@users.noreply.github.com>
2025-11-24 10:04:37 -0600

    Increased invalidator spawn cost

--- RiskyFixes/Fixes/Enemies/SolusSpawnrates.cs
+++ RiskyFixes/Fixes/Enemies/SolusSpawnrates.cs
@@ -16,6 +16,7 @@ namespace RiskyFixes.Fixes.Enemies
             SetSpawnCost(Addressables.LoadAssetAsync<CharacterSpawnCard>("RoR2/DLC3/WorkerUnit/cscWorkerUnit.asset").WaitForCompletion(), 16);  //Prospector 11->16, 14.5->10
             SetSpawnCost(Addressables.LoadAssetAsync<CharacterSpawnCard>("RoR2/DLC3/Tanker/cscTanker.asset").WaitForCompletion(), 35);  //Scorcher 18->35, 9.72 -> 5
             SetSpawnCost(Addressables.LoadAssetAsync<CharacterSpawnCard>("RoR2/DLC3/MinePod/cscMinePod.asset").WaitForCompletion(), 52);  //Distributor 20->52, 13 -> 5
+            SetSpawnCost(Addressables.LoadAssetAsync<CharacterSpawnCard>("RoR2/DLC3/DefectiveUnit/cscDefectiveUnit.asset").WaitForCompletion(), 80);  //Invalidator 40->80, 10 -> 5
         }
 
         private void SetSpawnCost(CharacterSpawnCard csc, int cost)
--- RiskyFixes/RiskyFixesPlugin.cs
+++ RiskyFixes/RiskyFixesPlugin.cs
@@ -21,7 +21,7 @@ namespace RiskyFixes
     [BepInDependency(R2API.R2API.PluginGUID)]
 
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
-    [BepInPlugin("com.Moffein.RiskyFixes", "RiskyFixes", "1.9.1")]
+    [BepInPlugin("com.Moffein.RiskyFixes", "RiskyFixes", "1.9.2")]
     public class RiskyFixesPlugin : BaseUnityPlugin
     {
         private void Awake()
